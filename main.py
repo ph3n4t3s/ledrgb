047ddc7d61d50ac7752cb910f8082c06cd62ae2a
ph3n4t3s <105867089+ph3n4t3s@users.noreply.github.com>
2025-01-28 02:44:20 +0100

    Mise à jour pxt.json, main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 /**
+ * Version analogique
+ * 
+ * Varie de 0 à 1023
+ */
+/**
+ * Version analogique
+ * 
+ * Varie de 0 à 1023
+ * 
+ * A REMPLACER dans le bloc au démarrage
+ */
+/**
  * La led est connectée à 3 broches (ports)
  * 
  * - P0 : La couleur rouge
@@ -7,38 +19,14 @@
  * 
  * - P2 : La couleur bleue
  */
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
 /**
  * Version numérique
  * 
  * Varie entre 0 à 1
  */
-/**
- * Version analogique
- * 
- * Varie de 0 à 1023
- */
-/**
- * Autre version numérique
- * 
- * Varie entre 0 à 1
- * 
- * Montrant les 8 possiblités
- */
-function setRGB (r: number, g: number, b: number) {
-    pins.digitalWritePin(DigitalPin.P0, r)
-    pins.analogWritePin(AnalogPin.P1, g)
-    pins.analogWritePin(AnalogPin.P2, b)
-}
-/**
- * Version analogique
- * 
- * Varie de 0 à 1023
- * 
- * A REMPLACER dans le bloc au démarrage
- */
-pins.digitalWritePin(DigitalPin.P0, 0)
-pins.digitalWritePin(DigitalPin.P1, 0)
-pins.digitalWritePin(DigitalPin.P2, 0)
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P0, 1)
     basic.pause(200)
